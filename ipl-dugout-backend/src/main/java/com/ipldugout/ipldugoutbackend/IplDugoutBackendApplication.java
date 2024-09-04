349fb02557945b84a7c06e5323d7bd3727ead547
Sathishkumar <sathishskvnr@gmail.com>
2024-09-05 01:34:06 +0530

    Set Up Java Application

---
+++ ipl-dugout-backend/src/main/java/com/ipldugout/ipldugoutbackend/IplDugoutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ipldugout.ipldugoutbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDugoutBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IplDugoutBackendApplication.class, args);
+    }
+
+}
